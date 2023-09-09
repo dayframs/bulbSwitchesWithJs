eb348606f68627ca52d84dfebef3e9db4d0bc8b3
dayframs <dayframs09@yahoo.com>
2023-09-09 04:29:14 +0100

    first commit

---
+++ bulb.js
@@ -0,0 +1,9 @@
+function switchBulb() {
+     if (btn3.innerText=="on") {
+               displayBulb.src ="onbulb.jpg";
+               btn3.innerText="off";
+            } else {
+               displayBulb.src="offbulb.jpg";
+               btn3.innerText="on";
+            }  
+} 
